ec717146b0ca0da53a967a35f7bdbc928aa84a49
MinhAnh234 <minhanhpkpro@gmail.com>
2021-08-19 01:17:36 +0900

    abc

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist 
+
+git init 
+git add .
+git commit -m "2.0"
+git push -f git@github.com:MinhAnh234/nike-vueJS.git gh repo clone MinhAnh234/nike-vueJS
+
+cd - 
