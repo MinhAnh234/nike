bada4b727e2adee3e6c45d02691149741c931096
MinhAnh234 <minhanhpkpro@gmail.com>
2021-08-23 10:57:07 +0900

    make it better

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ cd dist
 git init 
 git add .
 git commit -m "2.0"
-git push -f git@github.com:MinhAnh234/nike-vueJS.git gh repo clone MinhAnh234/nike-vueJS
+git remote add origin git@github.com:MinhAnh234/nike-vueJS.git dev
+git push
 
 cd - 
