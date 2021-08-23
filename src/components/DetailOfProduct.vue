78f0244413dbbed9c99b6e6797688ace77a0e5a7
MinhAnh234 <minhanhpkpro@gmail.com>
2021-08-23 12:58:16 +0900

    Heroku config

--- src/components/DetailOfProduct.vue
+++ src/components/DetailOfProduct.vue
@@ -418,9 +418,9 @@
                   $('.summary>span:nth-child(1)').toggleClass('isActive');
                   $('.text').toggleClass('d-block');
             });
-          /*   this.$store.dispatch("DetailOfProduct");
+           this.$store.dispatch("DetailOfProduct");
              this.$store.dispatch("detail_images_src");
-             this.$store.dispatch("getSizes"); */
+             this.$store.dispatch("getSizes");
              this.DetailOfProduct_id_array=[];
              if(this.DetailOfProduct.id==this.$route.params.idDetail){
                     $( ".childImage" ).attr('id', 'currentImage');
